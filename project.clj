4b77af784e5824b63273b4bd2919732cc8130825
Dennis Lipovsky <dennis.lipovsky@gmail.com>
2015-11-30 14:34:04 -0500

    adding explicit dependency on clojure/data.json since it was moved out of core

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-s3-static-deploy "0.1.1-SNAPSHOT"
+(defproject lein-s3-static-deploy "0.1.2-SNAPSHOT"
   :description "Leiningen plugin to deploy static websites"
   :url "https://github.com/ThoughtWorksInc/lein-s3-static-deploy"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-aws-s3 "0.3.7"]
                  [me.kanej/s3-sync "0.4.0"]
-                 [org.clojure/java.data "0.1.1"]]
+                 [org.clojure/java.data "0.1.1"]
+                 [org.clojure/data.json "0.2.6"]]
   :eval-in-leiningen true
   :min-lein-version "2.0.0"
   :scm {:name "git"
